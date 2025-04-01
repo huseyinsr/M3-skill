d42617ffbe5feb6bc0d9ca8db8e2f54f618390b6
huseyinsr <39071@ma-web.nl>
2025-04-01 20:00:50 +0200

    ,.,,,
    ng

---
+++ Assets/prefab/bit/Bit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bit : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+    public bool state = false;
+            
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (state)
+        {
+            spriteRenderer.color = Color.green;
+        }
+        else
+        {
+            spriteRenderer.color = Color.red;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        state = !state;
+    }
+}
