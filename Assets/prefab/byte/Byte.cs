94fdbd838060f6601138476d83452a2cc1876b0b
huseyinsr <39071@ma-web.nl>
2025-04-01 21:04:30 +0200

    f
    fdg

--- Assets/prefab/byte/Byte.cs
+++ Assets/prefab/byte/Byte.cs
@@ -7,7 +7,7 @@ public class Byte : MonoBehaviour
     [SerializeField] Bit[] bits = new Bit[8];
     [SerializeField] int value = 0;
 
-    void start()
+    void Start() // Fixed the capitalization to Start() instead of start()
     {
 
     }
@@ -27,6 +27,95 @@ public class Byte : MonoBehaviour
         if (bits[4].state) { value += 16; }
         if (bits[5].state) { value += 32; }
         if (bits[6].state) { value += 64; }
-        if (bits[7].state) { value = 128; }
+        if (bits[7].state) { value += 128; }
+    } // Added the missing closing brace here
+
+    public void SetByte(int newValue)
+    {
+        if (newValue > 255)
+        {
+            newValue = 255;
+        }
+
+        value = newValue;
+
+        if (newValue >= 128)
+        {
+            bits[7].state = true;
+            newValue -= 128;
+        } else
+        {
+            bits[7].state = false;
+        }
+
+        if (newValue >= 64)
+        {
+            bits[6].state = true;
+            newValue -= 64;
+        }
+        else
+        {
+            bits[6].state = false;
+        }
+
+        if (newValue >= 32)
+        {
+            bits[5].state = true;
+            newValue -= 32;
+        }
+        else
+        {
+            bits[5].state = false;
+        }
+
+        if (newValue >= 16)
+        {
+            bits[4].state = true;
+            newValue -= 16;
+        }
+        else
+        {
+            bits[4].state = false;
+        }
+
+        if (newValue >= 8)
+        {
+            bits[3].state = true;
+            newValue -= 8;
+        }
+        else
+        {
+            bits[3].state = false;
+        }
+
+        if (newValue >= 4)
+        {
+            bits[2].state = true;
+            newValue -= 4;
+        }
+        else
+        {
+            bits[2].state = false;
+        }
+
+        if (newValue >= 2)
+        {
+            bits[1].state = true;
+            newValue -= 2;
+        }
+        else
+        {
+            bits[1].state = false;
+        }
+
+        if (newValue >= 1)
+        {
+            bits[0].state = true;
+            newValue -= 1;
+        }
+        else
+        {
+            bits[0].state = false;
+        }
     }
 }
